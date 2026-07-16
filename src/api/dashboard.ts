c71fea42e5de55e5c9c4c29cb0626572b5627deb
test <test.gmail.com>
2026-07-17 01:51:38 +0300

    Revert "Update files"
    This reverts commit 1d552f6dbd712515aeb0bf8f6348a9c359d8c62e.

--- src/api/dashboard.ts
+++
@@ -1,8 +0,0 @@
-import api from './client'
-import type { DashboardStats } from '../types'
-
-// Статистика для дашборда
-export async function getDashboardStats(): Promise<DashboardStats> {
-  const { data } = await api.get<DashboardStats>('/dashboard/stats')
-  return data
-}
--- src/api/users.ts
+++
@@ -1,8 +0,0 @@
-import api from './client'
-import type { User } from '../types'
-
-// Список всех менеджеров (для фильтра)
-export async function getManagers(): Promise<User[]> {
-  const { data } = await api.get<User[]>('/users')
-  return data
-}
